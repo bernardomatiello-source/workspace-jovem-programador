a6597c68dabc2a456cc479f463db8c5c1f60bba1
bernardomatiello <bernardom.matiello@gmail.com>
2026-03-31 19:47:41 -0300

    Adicionado o Desafio 4

---
+++ 4 Prática 31-03/Desafio 4.js
@@ -0,0 +1,16 @@
+const select = document.getElementById('bebidaSelect')
+const display = document.getElementById('mensagemSaida')
+
+const frases = {
+    coffee: 'Boa escolha! Café é ótimo para acordar! ☕',
+    tea: 'Excelente! Um chá traz paz e relaxamento. 🍵',
+    juice: 'Saudável! Suco de fruta é pura energia! 🍹'
+}
+
+function displayFrase() {
+    const escolha = select.value
+    display.innerText = frases[escolha]
+    display.style.display = 'block'
+}
+
+select.addEventListener('change', displayFrase)
