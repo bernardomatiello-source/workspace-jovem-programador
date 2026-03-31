6a8c25ab4e4c28a8d2e3a27ea0f3a1d8c94c68f3
bernardomatiello <bernardom.matiello@gmail.com>
2026-03-30 21:52:52 -0300

    Adicionado Parte da 3 Prática

---
+++ 3 Prática 30-03/Desafio 3.js
@@ -0,0 +1,34 @@
+// Verificação de Elegibilidade
+const idadeDaPessoa = document.getElementById('personAge')
+const verificacao = document.getElementById('verification')
+
+function eligibilityVerification() {
+    const idade = parseFloat(idadeDaPessoa.value)
+    if(idade >= 18) {
+        verificacao.value = `Pode Votar`
+    } else {
+        verificacao.value = `Não Pode Votar`
+    }
+}
+
+idadeDaPessoa.addEventListener('input', eligibilityVerification)
+
+// Calcular o IMC
+const pesoDaPessoa = document.getElementById('weight')
+const alturaDaPessoa = document.getElementById('height')
+const IMC = document.getElementById('IMC')
+
+function calculateIMC() {
+    const peso = pesoDaPessoa.value
+    const altura = alturaDaPessoa.value
+    
+    if(altura <= 0) {
+        IMC.value = "Sem Valor de IMC"
+        return
+    }
+
+    IMC.value = `${(peso / (altura * altura)).toFixed(2)} de IMC`
+}
+
+pesoDaPessoa.addEventListener('input', calculateIMC)
+alturaDaPessoa.addEventListener('input', calculateIMC)
