5a4a35b33d50d208469a7137e662ddf008f84519
bernardomatiello <bernardom.matiello@gmail.com>
2026-05-04 21:46:56 -0300

    Adicionado refaturamentos nos testes

--- JP - TESTES DE SISTEMAS/Teste Calculadora - 22-04/calculadora.test.js
+++ JP - TESTES DE SISTEMAS/Teste Calculadora - 22-04/calculadora.test.js
@@ -1,4 +1,4 @@
-function testarCalculadora(nome, callback) {
+function testesCalculadora(nome, callback) {
     try {
         callback();
         console.log(`${nome}: Passou`);
@@ -7,18 +7,30 @@ function testarCalculadora(nome, callback) {
     }
 } 
 
-testarCalculadora('Somar Positivos', () => {
+// Testar as Operações
+testesCalculadora('Somar Positivos', () => {
     if (calculator(10, 20, 'sum') !== '30.00') throw new Error('Esperado 30')
 })
     
-testarCalculadora('Multiplicar Positivos', () => {
+testesCalculadora('Multiplicar Positivos', () => {
     if (calculator(10, 20, 'multiply') !== '200.00') throw new Error('Esperado 200')
 })
 
-testarCalculadora('Dividir Positivos', () => {
+testesCalculadora('Dividir Positivos', () => {
     if (calculator(20, 10, 'division') !== '2.00') throw new Error('Esperado 2')
 })
 
-testarCalculadora('Subtracao Positivos', () => {
+testesCalculadora('Subtracao Positivos', () => {
     if (calculator(20, 10, 'subtraction') !== '0') throw new Error('Esperado 10')
+})
+
+// Testar os Elementos numeros
+testesCalculadora('Existe o number1', () => {
+    if(num === null) throw new Error('O Elemento não existe')
+        const num = document.getElementById('number1')
+})
+
+testesCalculadora('Existe o number2', () => {
+    const num = document.getElementById('number2')
+    if(num === null) throw new Error('O Elemento não existe')
 })
